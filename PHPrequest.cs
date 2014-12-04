e72d8adf2be73ce637a67eea6154302afb23c7a6
Ayco <ayco@live.nl>
2014-12-04 22:09:03 +0100

    Simpel console programma om phprequests uit te voeren

---
+++ PHPrequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+
+namespace PHPrequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "http://harbingerofme.info/GnF/test.php";
+            WebClient wc = new WebClient();
+            NameValueCollection data = new NameValueCollection();
+            data["naam"] = "user";
+            data["email"] = "mail";
+            byte[] responseBytes = wc.UploadValues(URL, "POST", data);
+            string responsefromserver = Encoding.UTF8.GetString(responseBytes);
+            Console.WriteLine(responsefromserver);
+            Console.ReadKey();
+            wc.Dispose();
+        }
+    }
+}
+
+
+
